84cfaaf2fa6dfd4a41c797fa60c528ccb1a3eb9d
xukai <3353212714@qq.com>
2022-03-30 14:55:05 +0800

    gx

--- js/script.js
+++ js/script.js
@@ -174,12 +174,12 @@ onload = function() {
     };
 };
 
-/* 禁止右键，F12，F5 */
+/* 禁止右键，F12 */
 function unmouse(){
 	document.oncontextmenu = new Function("return false;");
 	document.onkeydown = document.onkeyup = document.onkeypress = function(event) {
 		var e = event || window.event || arguments.callee.caller.arguments[0];
-		if (e && (e.keyCode == 123 || (e.keyCode == 116 && e.type!='keypress'))) 
+		if (e && (e.keyCode == 123 || (e.keyCode == 116 && e.type!='keypress')))
 		{
 			e.returnValue = false;
 			return (false);
